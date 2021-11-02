d80de5b072bf91a55c352bc75cce50c1291b3865
Trivium-bit <trivium.exe@gmail.com>
2021-11-02 15:30:47 +0300

    projects

--- src/projects/project/Project.js
+++ src/projects/project/Project.js
@@ -5,7 +5,8 @@ import style from './Project.module.css';
 function Project(props) {
   return (
     <div className={style.project}>
-    <div className={style.icon}><img src="https://example.com/photo/low.jpg" alt={props.title}></img> </div>
+    <div className={style.img}>
+    <img src="https://intersog.com/wp-content/uploads/2017/04/Screen-Shot-2017-04-14-at-3.44.02-PM-e1492176136977.png" alt={props.title}></img> </div> 
     <h4>{props.title}</h4>
   <span className={style.description}>{props.description}</span>
     </div>
