f4f0b6232692b56c3e84f994f016038c4ce7a824
Trivium-bit <trivium.exe@gmail.com>
2021-11-04 12:50:44 +0300

    fix_adaptive

--- src/projects/project/Project.js
+++ src/projects/project/Project.js
@@ -5,9 +5,9 @@ import style from './Project.module.css';
 function Project(props) {
   return (
     <div className={style.project}>
-    <div className={style.img}>
+    <div className={style.imgContainer}>
     <img src={props.img} alt={props.alt}></img>
-    <a href={props.url} className={style.button}>Переход по ссылке</a>
+    <a href={props.url} className={style.button}>VIEW</a>
     </div> 
     <h4>{props.title}</h4>
     <span className={style.description}>{props.description}</span>
