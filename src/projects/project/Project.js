40dc7f53319272be9a9f5403029be7174dea31be
Trivium-bit <trivium.exe@gmail.com>
2021-11-09 10:32:18 +0300

    fix_img_projects

--- src/projects/project/Project.js
+++ src/projects/project/Project.js
@@ -6,14 +6,12 @@ function Project(props) {
   return (
     <div className={style.project}>
     <div className={style.imgContainer}>
-    <img className={style.img} src={props.img} alt={props.alt}></img>
+    <div className={style.img} src={props.img}></div>
     <a href={props.url} className={style.button}>VIEW</a>
     </div> 
     <h4>{props.title}</h4>
     <span className={style.description}>{props.description}</span>
-    
     </div>
-   
   );
 }
 
