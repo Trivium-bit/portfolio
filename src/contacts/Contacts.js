d63f21d8c794d3959c97e19382a44349c82683c3
Trivium-bit <trivium.exe@gmail.com>
2022-03-16 13:20:30 +0300

    Contacts

--- src/contacts/Contacts.js
+++ src/contacts/Contacts.js
@@ -5,16 +5,16 @@ import styleContainer from './../common/styles/Container.module.css'
 function Contacts() {
   return (
     <div className={style.contactsBlock}>
-    <div className={`${styleContainer.container} ${style.contactsContainer}`}>        
-    <span className={style.title}>Contacts</span>
-    <form className={style.form}>
-      <input placeholder="Name"></input>
-      <input placeholder="E-mail"></input>
-      <textarea placeholder="message"></textarea>
-    </form>
-    <button className={style.button}>Send</button>
-     
-    </div>
+      <div className={`${styleContainer.container} ${style.contactsContainer}`}>
+        <span className={style.title}>Contacts</span>
+        <form className={style.form}>
+          <input type="text" placeholder="Name"></input>
+          <input type="text" placeholder="E-mail"></input>
+          <textarea placeholder="message"></textarea>
+        </form>
+        <button type="submit" className={style.button}>Send</button>
+
+      </div>
     </div>
   );
 }
