73949515db3b8173d38bd365af28a2440028aa0e
Trivium-bit <trivium.exe@gmail.com>
2022-03-29 12:33:04 +0300

    projects_images

--- src/projects/Projects.js
+++ src/projects/Projects.js
@@ -3,33 +3,43 @@ import style from './Projects.module.css';
 import styleContainer from './../common/styles/Container.module.css'
 import Project from './project/Project'
 import Title from '../common/styles/components/Title';
+import SocialImage from '../assets/image/social_network.jpg'
+import TodoImage from '../assets/image/Todolist.jpg'
 
 const projets = {
-  project1: {
+  socialNetwork: {
     url: "https://intersog.com/wp-content/uploads/2017/04/Screen-Shot-2017-04-14-at-3.44.02-PM-e1492176136977.png",
     title: "Social Network",
-    description: "JavaScript is a scripting language that enables you to create dynamically updating content, control multimedia, animate images..."
+    description: "JavaScript is a scripting language that enables you to create dynamically updating content, control multimedia, animate images...",
   },
   project2: {
     url: "https://intersog.com/wp-content/uploads/2017/04/Screen-Shot-2017-04-14-at-3.44.02-PM-e1492176136977.png",
     title: "Todolist",
-    description: "Cascading Style Sheets (CSS) is a style sheet language used for describing the presentation of a document written in a markup language such as HTML."
+    description: "Cascading Style Sheets (CSS) is a style sheet language used for describing the presentation of a document written in a markup language such as HTML.",
   },
   project3: {
     url: "https://intersog.com/wp-content/uploads/2017/04/Screen-Shot-2017-04-14-at-3.44.02-PM-e1492176136977.png",
     title: "Weather Forecast",
-    description: "This is where things get a little fuzzy and you better have your lexical scope wits about ... So what does this mean for React."
+    description: "This is where things get a little fuzzy and you ",
   }
 }
 
 function Projects() {
+  
+  const social = {
+    backgroundImage: `url(${SocialImage})`,
+  };
+  const todolist = {
+    backgroundImage: `url(${TodoImage})`,
+  };
+
   return (
     <div className={style.projectsBlock}>
       <div className={`${styleContainer.container} ${style.projectsContainer}`}>
-        <Title text={"Projets"} description={"my latest projects "}/>
+        <Title text={"Projets"} description={"my latest projects "} />
         <div className={style.projects}>
-          <Project title={projets.project1.title} description={projets.project1.description} url={projets.project1.url} />
-          <Project title={projets.project2.title} description={projets.project2.description} url={projets.project2.url} />
+          <Project style={social} title={projets.socialNetwork.title} description={projets.socialNetwork.description} url={projets.socialNetwork.url} />
+          <Project style={todolist} title={projets.project2.title} description={projets.project2.description} url={projets.project2.url} />
           <Project title={projets.project3.title} description={projets.project3.description} url={projets.project3.url} />
         </div>
       </div>
--- src/projects/project/Project.js
+++ src/projects/project/Project.js
@@ -5,12 +5,11 @@ import style from './Project.module.css';
 function Project(props) {
   return (
     <div className={style.project}>
-    <div className={style.imgContainer}>
-    <div className={style.img} src={props.img}></div>
-    <a href={props.url} className={style.button}>VIEW</a>
-    </div> 
-    <h4 className={style.projectTitle}>{props.title}</h4>
-    <span className={style.description}>{props.description}</span>
+      <div className={style.imgContainer} style={props.style}>
+        <a href={props.url} className={style.button}>VIEW</a>
+      </div>
+      <h4 className={style.projectTitle}>{props.title}</h4>
+      <span className={style.description}>{props.description}</span>
     </div>
   );
 }
