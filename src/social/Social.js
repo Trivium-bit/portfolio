e99935fcef96fecc89051e74a5b9574b2ffa6ba8
Trivium-bit <trivium.exe@gmail.com>
2022-04-07 09:05:52 +0300

    links social

--- src/social/Social.js
+++ src/social/Social.js
@@ -6,8 +6,8 @@ import { Icon } from '@iconify/react';
 function Social() {
   return (
     <div className={style.social}>
-    <li><a href=" "><Icon icon="akar-icons:instagram-fill" className={style.icon} width="24" height="24" /></a> </li>
-    <li><a href=" "><Icon icon="akar-icons:linkedin-fill" className={style.icon} width="24" height="24" /></a> </li>
+    <li><a href=" "><Icon icon="https://www.instagram.com/trivium.exe/b89d7s8as/" className={style.icon} width="24" height="24" /></a> </li>
+    <li><a href="https://www.linkedin.com/in/vitali-baliankou-b856051b9/"><Icon icon="akar-icons:linkedin-fill" className={style.icon} width="24" height="24" /></a> </li>
     <li><a href=" "><Icon icon="akar-icons:facebook-fill" className={style.icon} width="24" height="24" /></a> </li>
     <li><a href=" "><Icon icon="akar-icons:twitter-fill" className={style.icon} width="24" height="24" /></a> </li>
     </div>
