e9f3c16d7dc80b1a30e4a7aab5bd8c10e47f01cc
Trivium-bit <trivium.exe@gmail.com>
2022-09-10 09:36:12 +0300

    add particles

--- src/main/Main.js
+++ src/main/Main.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import style from './Main.module.scss';
+import Particles from 'react-particles-js';
 
 function Main() {
   return (
     <div className={style.mainBlock}>
+       <Particles />
       <div className={style.mainContainer}>
         <div className={style.greetings}>
           <span>Hi There</span>
--- src/skills/Skills.js
+++ src/skills/Skills.js
@@ -3,72 +3,73 @@ import style from './Skills.module.scss';
 import styleContainer from './../common/styles/Container.module.scss'
 import Skill from './skill/Skill'
 import Title from '../common/components/title/Title';
-import { faReact, faJsSquare, faHtml5, faGitAlt, faCss3Alt} from '@fortawesome/free-brands-svg-icons';
-
-const skills = {
-  react: {
-    icon: faReact,
-    title: "React",
-    description: "JavaScript is a scripting languaanguage that en enables you to create dynamically updating content, control multimedia, animate images...",
-  },
-  javascript: {
-    icon: faJsSquare,
-    title: "JavaScript",
-    description: "Cascading Style Sheets (CSS) is a style sheet language used for describingguage used foon of a document written in a markup guage used foTML.",
-  },
-  typescript: {
-    logo: "simple-icons:typescript",
-    title: "TypeScript",
-    description: "is a style sheet language used for describing the presentation of",
-  },
-  redux: {
-    logo: "akar-icons:redux-fill",
-    title: "Redux",
-    description: "iguage used fonguage used forguage used foguage used fotianguage thaables you to createting conteting conteot is a scriptn of",
-  },
-  gitHub: {
-    icon: faGitAlt,
-    title: "Git",
-    description: "heets (Cge used fogn style sheet lage used fogndescribing the presentation of a d",
-  }, 
-  html5: {
-    icon: faHtml5,
-    title: "HTML 5",
-    description: "This is where things get a little fuzzy and you ",
-  },
-  restAPI: {
-    logo: "dashicons:rest-api",
-    title: "REST API",
-    description: "Thiadasdasdas is whesdasdasdasre things get a little fuzzy and you ",
-  },
-  css: {
-    icon: faCss3Alt,
-    title: "CSS",
-    description: "eets (CSS) is a style sheet language ustyle sheet ge used fogng thege used fognused fogresu ",
-  },
-  materialUI: {
-    logo: "mdi:material-ui",
-    title: "MaterialUI",
-    description: "Axios tge used fogntyle sheet tyle sheege used fogntyle sheet ge used fognyle ge used fogn ",
-  },
-  styledComponents: {
-    logo: "file-icons:styledcomponents",
-    title: "Styled-components",
-    description: "Styled-components has quickly become my favorite library for React, so much so, that when I watch a tutoria",
-  },
-  antDesign: {
-    logo: "ant-design:ant-design-outlined",
-    title: "AntDesign",
-    description: "(CSS) is a style sheet language used for describingguaged fog(CSS) is a style sheet language used for describingguageeet ge used fognyle ge used fogn ",
-  },
-  postman: {
-    logo: "simple-icons:postman",
-    title: "Postman",
-    description: "s where things get a litsheet tyle ss where things get a lits  lits where things get a litused fogntyle sheet ge used fognyle ge used fogn ",
-  },
-}
+import { faReact, faJsSquare, faHtml5, faGitAlt, faCss3Alt } from '@fortawesome/free-brands-svg-icons';
 
 function Skills() {
+
+  const skills = {
+    react: {
+      icon: faReact,
+      title: "React",
+      description: "JavaScript is a scripting languaanguage that en enables you to create dynamically updating content, control multimedia, animate images...",
+    },
+    javascript: {
+      icon: faJsSquare,
+      title: "JavaScript",
+      description: "Cascading Style Sheets (CSS) is a style sheet language used for describingguage used foon of a document written in a markup guage used foTML.",
+    },
+    typescript: {
+      logo: "simple-icons:typescript",
+      title: "TypeScript",
+      description: "is a style sheet language used for describing the presentation of",
+    },
+    redux: {
+      logo: "akar-icons:redux-fill",
+      title: "Redux",
+      description: "iguage used fonguage used forguage used foguage used fotianguage thaables you to createting conteting conteot is a scriptn of",
+    },
+    gitHub: {
+      icon: faGitAlt,
+      title: "Git",
+      description: "heets (Cge used fogn style sheet lage used fogndescribing the presentation of a d",
+    },
+    html5: {
+      icon: faHtml5,
+      title: "HTML 5",
+      description: "This is where things get a little fuzzy and you ",
+    },
+    restAPI: {
+      logo: "dashicons:rest-api",
+      title: "REST API",
+      description: "Thiadasdasdas is whesdasdasdasre things get a little fuzzy and you ",
+    },
+    css: {
+      icon: faCss3Alt,
+      title: "CSS",
+      description: "eets (CSS) is a style sheet language ustyle sheet ge used fogng thege used fognused fogresu ",
+    },
+    materialUI: {
+      logo: "mdi:material-ui",
+      title: "MaterialUI",
+      description: "Axios tge used fogntyle sheet tyle sheege used fogntyle sheet ge used fognyle ge used fogn ",
+    },
+    styledComponents: {
+      logo: "file-icons:styledcomponents",
+      title: "Styled-components",
+      description: "Styled-components has quickly become my favorite library for React, so much so, that when I watch a tutoria",
+    },
+    antDesign: {
+      logo: "ant-design:ant-design-outlined",
+      title: "AntDesign",
+      description: "(CSS) is a style sheet language used for describingguaged fog(CSS) is a style sheet language used for describingguageeet ge used fognyle ge used fogn ",
+    },
+    postman: {
+      logo: "simple-icons:postman",
+      title: "Postman",
+      description: "s where things get a litsheet tyle ss where things get a lits  lits where things get a litused fogntyle sheet ge used fognyle ge used fogn ",
+    },
+  }
+
   return (
     <div className={style.skillsBlock}>
       <div className={`${styleContainer.container} ${style.skillsContainer}`}>
